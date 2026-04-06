0a75c891633c2eafb3b1754229f41c31eee2e25b
GAJIN-S <gajin9025@gmail.com>
2026-04-06 18:55:35 +0530

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -1,2 +1,9 @@
 def test_basic_math():
+    # A simple test to ensure pytest has something to collect and pass
     assert 2 + 2 == 4
+    
+def test_app_is_working():
+    # Another simple test
+    expected_status = "running"
+    current_status = "running"
+    assert current_status == expected_status
